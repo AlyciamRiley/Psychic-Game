54a67f5b81d63bd596fccf8d6c3f568a45d3baeb
Alycia Riley <alyciadahlenm@gmail.com>
2017-12-05 21:53:55 -0600

    don't give up yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,85 @@
 
 
+// declare variablessss
 
 
 
+// 1. Computer must generate random letter
 
 
-//Results need to be displayed
-//var wins = 0;
-//var losses = 0;
-//var guessesLeft = 0;
-
-
-
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
- var computerGuess = 
-     alphabet[Math.floor(Math.random() * alphabet.length)]
- console.log(computerGuess);
-
-    document.onkeyup = function() {
-    var userguess = 
-        String.fromCharCode(event.keyCode).toLowerCase();
+    var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var wins = 0;
+    var losses = 0;
+    //var letterIndex = ;
+    var guessesRemaining = 9;
+    
     
 
- 
+newRound();
+
+   
+function newRound() {
+//1
+    var computerGuess = 
+     computerChoices[Math.floor(Math.random() * computerChoices.length)];
+     console.log(computerGuess);
 
 
-   if (userguess == computerGuess) {
-     alert("You win!");
- 
-    }  if (userguess != computerGuess) {
-        alert("You lose!");
+// 2. User must choose first letter 
+
+
+document.onkeyup = function(event) {
+    var userGuess = 
+        String.fromCharCode(event.keyCode).toLowerCase();
+
+    console.log(userGuess);
+  
+//var winsContainer = document.getElementById('wins);
+
+// 3. Compare first guess with computer guess
+
+
+    if ((userGuess == computerGuess)) {
+        alert("You win!");
+        wins++;
+        
+    } else {
+        alert("Try again!");
     }
-    }
 
+}
 
-/*//computer randomly generates a letter 
-var computerGuess = 
-     alphabet[Math.floor(Math.random() * alphabet.length)]
-console.log(computerGuess);
-
-//user picks a letter
-document.onkeyup = function() {
-    var userguess = 
-    String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userguess);
-
-//compare results
+   
+    
+newRound(); 
+}
    
 
-  
-   if ((userguess != computerGuess)) {
-       alert("Try again!");
-   }*/
-    
-//track number of guesses
-    //for loop? 
-    
-//After 9 tries, user loses. 
- 
 
 
 
-//display results
-    //var html = "<h2>Guess which letter I'm thinking of! <h2>" +
-        //"<p>wins: " + wins + "</p>" +
-        //"<p>Losses: " + losses + "</p>" +
-       // "<p>guessesLeft: " + guessesLeft + "</p>";
+    // a. If it's correct
+        // wins goes up by 1 
+        //code starts over
+    // b. If its incorrect
+        // User guess is logged under "Letters Guessed"
+            //letterIndex++
+        // Number of guesses remaining goes down by 1
+
     
-    //document.querySelector('#game').innerHTML = html;
+
+
+
+// 4. Code repeats 9 times
+// 5. If user doesn't guess within 9 guesses    
+    // losses goes up by 1
+    // Code starts over
+
+
+
+
+
+
+
     
-//}
-    
- 
+
